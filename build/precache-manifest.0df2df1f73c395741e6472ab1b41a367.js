0ef6e3069409dc8d122a7ed1cfe71110f8c5c6b7
Briana King <brianaking@Brianas-Air.lan>
2024-09-04 11:41:50 +0100

    added callback to redirect uri

---
+++ build/precache-manifest.0df2df1f73c395741e6472ab1b41a367.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c583e0a469f2823c4e4481078caee0",
+    "url": "/cleanify2.0/index.html"
+  },
+  {
+    "revision": "e92f22051410cda4b09f",
+    "url": "/cleanify2.0/static/css/main.ed000563.chunk.css"
+  },
+  {
+    "revision": "0f08b76be80101f21f28",
+    "url": "/cleanify2.0/static/js/2.0cb6853e.chunk.js"
+  },
+  {
+    "revision": "333a23518a94c2d0bd79d9b28de1caef",
+    "url": "/cleanify2.0/static/js/2.0cb6853e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e92f22051410cda4b09f",
+    "url": "/cleanify2.0/static/js/main.2db0571d.chunk.js"
+  },
+  {
+    "revision": "7cce6407bbfa5575e807",
+    "url": "/cleanify2.0/static/js/runtime-main.11d661a2.js"
+  },
+  {
+    "revision": "30b581fcfcb45c44ec1adca7a40c5cd6",
+    "url": "/cleanify2.0/static/media/emptyPlaylist.30b581fc.png"
+  },
+  {
+    "revision": "856b7ae0fcb790bb8370db49d046d0b5",
+    "url": "/cleanify2.0/static/media/logo.856b7ae0.png"
+  },
+  {
+    "revision": "cc02a310228a0c83158dd5c96d803c69",
+    "url": "/cleanify2.0/static/media/profPic.cc02a310.png"
+  }
+]);
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const __dirname = path.resolve();
 var client_id = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 var client_secret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
 // var redirect_uri = 'http://localhost:3333/callback'; DEV
-var redirect_uri = 'https://briiking4.github.io/cleanify2.0/';
+var redirect_uri = 'https://briiking4.github.io/cleanify2.0/callback';
 
 
 var stateKey = 'spotify_auth_state';
